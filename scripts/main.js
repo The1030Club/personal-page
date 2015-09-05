f5b9c2468c2f3e73141b3d10e2b81ec5e7863ff3
Ben Haaland <bphaaland@gmail.com>
2015-09-05 12:48:36 -0400

    pushing to github repo

---
+++ scripts/main.js
@@ -0,0 +1,26 @@
+
+
+//jQuery FadeIn Module
+
+//var fadeInText = $( '#js-fadeInText' );
+//
+//// When the Document loads completely, fade in the text for 1500 ms
+//$( document ).ready(function() {
+//  fadeInText.fadeIn(4000);
+//});
+
+
+
+// Typed.js plug-in, credit and thanks to Matt Boldt!
+$(function(){
+    $(".js-typing-element").typed({
+        strings: [
+            "First sentence.",
+            "Second sentence.",
+            "Third sentence.",
+            "Fourth sentence.",
+            "Fifth sentence."
+        ],
+        typeSpeed: 100
+    });
+});
